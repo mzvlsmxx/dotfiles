9bb36c1c11ef2a15938b7640718596bcae132560
mzvlsmxx <mzvlsm.xx@gmail.com>
2025-12-20 12:51:29 +0300

    Initial

---
+++ bin/add_to_parh.sh
@@ -0,0 +1,3 @@
+#!/bin/env bash
+
+echo 'export PATH="$HOME/bin:$PATH"' >> ~/.bashrc
---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+
+# sddm theme
+sudo cp -r ~/dotfiles/sddm/mzvlsmxx-sddm/ /usr/share/sddm/themes/
+
+# other configs
+\cp -r ~/dotfiles/config/* ~/.config/
+
+# commands
+cp -r ~/dotfiles/bin ~/
