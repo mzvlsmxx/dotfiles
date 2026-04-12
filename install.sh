3e792b8552b3ca69527d19bd8afad3da311e0a9e
mzvlsmxx <mzvlsm.xx@gmail.com>
2026-04-12 10:09:50 +0300

    wip: 12.04.2026 10:09

--- install.sh
+++ install.sh
@@ -3,16 +3,12 @@
 theme=''
 
 while true; do
-    read -n 1 -p $'Select theme:\n1. Main\n2. Orange\n0. Cancel\n\n>> ' theme_num
+    read -n 1 -p $'Select theme:\n1. Main\n0. Cancel\n\n>> ' theme_num
     echo ""
 
     case $theme_num in
         1)
             theme='main'
-            break
-            ;;
-        2)
-            theme='orange'
             break
             ;;
         0)
@@ -26,22 +22,6 @@ while true; do
     esac
 done
 
-# case $theme_num in
-#     # 2|3)
-#     #     echo "Вы выбрали два или три"
-#     #     ;;
-#     # [4-7])
-#     #     echo "Вы выбрали цифру от 4 до 7"
-#     #     ;;
-#     # 0|8|9)
-#     #     echo "Вы выбрали 0, 8 или 9"
-#     #     ;;
-#     *)
-#         echo "Это не цифра или ввод пуст"
-#         ;;
-# esac
-
-
 
 # sddm theme
 sudo \cp -r $HOME/dotfiles/themes/$theme/sddm/mzvlsmxx-sddm/ /usr/share/sddm/themes/
@@ -49,9 +29,6 @@ sudo \cp -r $HOME/dotfiles/themes/$theme/sddm/mzvlsmxx-sddm/ /usr/share/sddm/the
 # other configs
 # sudo rsync -a --exclude 'hyprland' $HOME/dotfiles/themes/$theme/config/* $HOME/.config/
 \cp -r $HOME/dotfiles/themes/$theme/config/* $HOME/.config/
-
-# hyprland
-# \cp -r $HOME/dotfiles/themes/$theme/config/hyprland/* $HOME/.config/hypr/
 
 # commands
 \cp -r $HOME/dotfiles/bin $HOME/
