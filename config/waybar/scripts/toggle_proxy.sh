8b156af8d54040079f9ad1c0aeb42dd86082067c
mzvlsmxx <mzvlsm.xx@gmail.com>
2026-05-28 20:08:37 +0300

    wip: 28.05.2026 20:08

--- config/waybar/scripts/toggle_proxy.sh
+++ config/waybar/scripts/toggle_proxy.sh
@@ -3,25 +3,27 @@
 CONNECTION_ID="014f647a-2b55-4958-9006-21afa7b7aaf5"
 CONNECTION_NAME="OpenVPN"
 
-# Функция для вывода статуса
 print_status() {
-    if nmcli connection show --active | grep -q "$CONNECTION_ID"; then
+    if nmcli -f UUID,STATE connection show | grep "$CONNECTION_ID" | grep -q "activating"; then
+        echo "{\"alt\": \"activating\", \"text\": \"Proxy\", \"class\": \"proxy-activating\", \"tooltip\": \"Activating: $CONNECTION_NAME\nUUID: $CONNECTION_ID\"}"
+        pkill -RTMIN+1 waybar
+    elif nmcli connection show --active | grep -q "$CONNECTION_ID"; then
+        notify-send "Proxy activated" "Connected: $CONNECTION_NAME\nUUID: $CONNECTION_ID"
         echo "{\"alt\": \"on\", \"text\": \"Proxy\", \"class\": \"proxy-on\", \"tooltip\": \"Connected: $CONNECTION_NAME\nUUID: $CONNECTION_ID\"}"
     else
+        notify-send "Proxy deactivated" "Disconnected: $CONNECTION_NAME\nUUID: $CONNECTION_ID"
         echo "{\"alt\": \"off\", \"text\": \"Proxy\", \"class\": \"proxy-off\", \"tooltip\": \"Disconnected\"}"
     fi
 }
 
-# Если передан аргумент "toggle" (нажатие в Waybar)
 if [[ "$1" == "toggle" ]]; then
     if nmcli connection show --active | grep -q "$CONNECTION_ID"; then
-        nmcli connection down "$CONNECTION_ID" > /dev/null
+        nmcli connection down "$CONNECTION_ID" > /dev/null &
     else
-        nmcli connection up "$CONNECTION_ID" > /dev/null
+        notify-send "Proxy is activating" "Activating: $CONNECTION_NAME\nUUID: $CONNECTION_ID"
+        nmcli connection up "$CONNECTION_ID" > /dev/null &
     fi
-    # Отправляем сигнал Waybar обновить модуль (RTMIN+10 соответствует signal: 10)
     pkill -RTMIN+1 waybar
 else
-    # Если запущен без аргумента (первый запуск Waybar)
     print_status
 fi
